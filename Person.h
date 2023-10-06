3c677f45577e4189400f53f6af634eea583d0371
givoxxs <toanyogame@gmail.com>
2023-10-06 10:48:57 +0700

    Add Person.h

---
+++ Person.h
@@ -0,0 +1,18 @@
+// Person.h khai báo
+#ifndef PERSON_H
+#define PERSON_H
+#include <iostream>
+using namespace std;
+
+class Person {
+    int id;
+    string name, phoneNumber;
+
+    public: 
+        Person();
+        ~Person();
+
+        Person Add(const Person& other);
+        Person Display();
+        void Search();
+}
