00ef1e476a7691ad5ecc2b62b0a2375794c09a4c
givoxxs <toanyogame@gmail.com>
2023-10-06 11:05:06 +0700

    Add Person.cpp

---
+++ Person.cpp
@@ -0,0 +1,9 @@
+#include "Person.h"
+#include <iostream>
+using namespace std;
+
+Person::Person() {}
+Person::~Person() {}
+Person Person::Add(const Person& other) {}
+Person Person::Display() {}
+void Person::Search() {}
