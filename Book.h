66118a5936f140b549d44a467d0effcc67905812
nhattuan2402 <nhattuan242@gmail.com>
2023-10-06 10:35:16 +0700

    Add Book.h

---
+++ Book.h
@@ -0,0 +1,24 @@
+#ifndef BOOK_H
+#define BOOK_H
+class person {
+
+};
+
+class employee:public person {
+
+};
+
+class customer:public person {
+
+};
+
+class book {
+
+};
+
+class order {
+
+};
+
+
+#endif
