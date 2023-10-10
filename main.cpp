538c3cfff60da9684a7aa5062f599c68ba719970
givoxxs <toanyogame@gmail.com>
2023-10-10 17:09:51 +0700

    Something to fix complete

--- main.cpp
+++ main.cpp
@@ -1,12 +1,41 @@
 #include "Person.h"
-#include <iostream>
+#include "Customer.h"
 
+#include <iostream>
 using namespace std;
-Person personList[100];
 
 int main()
 {
     /* code */
-    cout << "hello world";
+    // cout << "hello world";
+    // return 0;
+    // Create some people
+    Person person1(1, "John Doe", "123-456-7890");
+    Person person2(2, "Alice Smith", "987-654-3210");
+    Person person3(3, "Bob Johnson", "555-555-5555");
+
+    // Create some customers
+    Customer customer1(101, "Customer A", "111-111-1111");
+    Customer customer2(102, "Customer B", "222-222-2222");
+
+    // Display all people
+    cout << "All People:" << endl;
+    Person::DisplayAll();
+    cout << "-------------------------" << endl;
+
+    // Search for people by name
+    cout << "People with name 'Alice':" << endl;
+    Person::SearchByName("Alice");
+    cout << "-------------------------" << endl;
+
+    // Search for people by phone number
+    cout << "People with phone number '555-555-5555':" << endl;
+    person3.Search("555-555-5555");
+    cout << "-------------------------" << endl;
+
+    // Order books for customers
+    customer1.OrderBook();
+    customer2.OrderBook();
+
     return 0;
 }
